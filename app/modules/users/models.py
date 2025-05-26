38c45b1d87314bf869f7161e847c742a8b06cea8
Khalid Mim Muzahid <khalidmimm@gmail.com>
2025-05-26 22:49:50 +0600

    developed user (for admin) module for sign up/in

---
+++ app/modules/users/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Enum
+from database import Base
+import enum
+
+
+# Define an enumeration for role type
+class UserRoleEnum(enum.Enum):
+    admin = "admin"
+    # user =  "user"
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    role = Column(Enum(UserRoleEnum), nullable=False, default= "admin")
+    name = Column(String,  unique=False, index=True)
+    password = Column(String, nullable=False)
