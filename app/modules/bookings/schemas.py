67c9fad8708955de015d0fe5d3998bf8678d4f5a
Khalid Mim Muzahid <khalidmimm@gmail.com>
2025-05-26 16:30:31 +0600

    developed schemas.py for booking module

---
+++ app/modules/bookings/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from enum import Enum
+from services.schemas import ServiceReferenceResponseForBookingService
+
+class StatusTypeEnum(str, Enum):
+    pending = "pending" #Booking request has been made but not yet confirmed.
+    confirmed = "confirmed" # Booking is accepted and scheduled.
+    in_progress= "in_progress" # The service is currently being provided.
+    completed = "Completed" # The service has been successfully completed.
+    cancelled="cancelled"   #Booking was cancelled by the user or provider.
+
+class BookingCreateRequest(BaseModel):
+    customer_name : str
+    customer_phone: str
+    service_id: str
+
+class BookingCreateResponse(BaseModel):
+    id: int
+    customer_name : str
+    customer_phone: str
+    status: StatusTypeEnum
+    service : ServiceReferenceResponseForBookingService
+    class Config:
+        orm_mode = True
+        extra = "ignore"
+
+
+class BookingListResponse(BookingCreateRequest):
+    # extra_field: str  # Add extra fields if needed
+    pass
